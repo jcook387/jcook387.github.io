2bc453059f2cb21262a67c1ec02959de9f2463ae
jcook387 <123977523+jcook387@users.noreply.github.com>
2023-04-27 15:37:32 -0500

    Added unit 11 to page

---
+++ unit11/submit-brochure-request.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Your request for a brochure has been processed!</h1>
+<img src="https://th.bing.com/th/id/OIP.DbA7_sgVYhfgqUIrYopxXwHaHR?pid=ImgDet&rs=1" alt="thumbs up" style="height=50%";><br>
+<br><button onclick="goBack()">Go Back</button>
+
+<script>
+function goBack() {
+  history.go(-2);
+}
+</script>
+
+</body>
+</html>
